99adf731ef9de4b837bc08ef7ef189dee3421b21
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 12:06:37 +0530

    Added apps and dependencies in installed apps

--- price_alert_application/price_alert_application/settings.py
+++ price_alert_application/price_alert_application/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_api_framework',
+    'rest_framework',
     'price_alert_api',
 ]
 
