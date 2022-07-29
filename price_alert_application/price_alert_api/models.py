c0d12c8c7105d77a43eaf05310acb27f69f61d99
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 12:27:50 +0530

    Added user model for authentication

--- price_alert_application/price_alert_api/models.py
+++ price_alert_application/price_alert_api/models.py
@@ -4,3 +4,7 @@ class Alert(models.Model):
    name = models.CharField(max_length=100)
    price = models.IntegerField()
    status = models.CharField(max_length=50)
+
+class User(models.Model):
+   name = models.CharField(max_length=100)
+   authCode = models.TextField()
