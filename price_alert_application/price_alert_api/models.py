a68f43d2834f595a1d270feb9206eb4dec495a17
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 12:26:02 +0530

    Added model

--- price_alert_application/price_alert_api/models.py
+++ price_alert_application/price_alert_api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Alert(models.Model):
+   name = models.CharField(max_length=100)
+   price = models.IntegerField()
+   status = models.CharField(max_length=50)
