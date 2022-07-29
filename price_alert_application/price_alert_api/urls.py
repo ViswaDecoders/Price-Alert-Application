8c35aa69cdafe27e6d3ee5ae8949e24b6b964b58
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 17:58:36 +0530

    Created user Login and Create urls

--- price_alert_application/price_alert_api/urls.py
+++ price_alert_application/price_alert_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Alert_Api
+from .views import Alert_Api,User_Create,User_Login
 
 urlpatterns = [
+    path('users/create', User_Create.as_view()),
+    path('users/Login', User_Login.as_view()),
     path('alerts/create', Alert_Api.as_view()),
     path('alerts', Alert_Api.as_view()),
     path('alerts/delete<int:item_id>', Alert_Api.as_view()),
-    
 ]
