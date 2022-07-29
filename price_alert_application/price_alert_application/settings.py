4ff028cefb6634159bf7700d17aae7602ce94f4e
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 22:23:32 +0530

    Final commit

--- price_alert_application/price_alert_application/settings.py
+++ price_alert_application/price_alert_application/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alapativiswanath1@gmail.com'
-EMAIL_HOST_PASSWORD = 'bbrflefmfsfzqqjg'
+EMAIL_HOST_USER = env('HOST_EMAIL')
+EMAIL_HOST_PASSWORD = env('HOST_PWD')
