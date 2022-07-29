d0b8783668a1ec4a46de045ab5fd5fea305c541e
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 12:37:51 +0530

    Added serializers.py

---
+++ price_alert_application/price_alert_api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from price_alert_api.models import Alert, User
+
+class AlertSerializer(serializers.ModelSerializer):
+   class Meta:
+       model = Alert
+       fields = ('name', 'price', 'status')
+
+
+class UserSerializer(serializers.ModelSerializer):
+   class Meta:
+       model = User
+       fields = ('name')
