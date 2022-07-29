27c32677a2aa75486383e7614462c5b5df18934b
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 17:57:27 +0530

    Added user in alert model

--- price_alert_application/price_alert_api/models.py
+++ price_alert_application/price_alert_api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Alert(models.Model):
-   name = models.CharField(max_length=100)
-   crytoCurrency = models.CharField(max_length=100)
-   price = models.FloatField()
-   status = models.CharField(max_length=50)
+    user = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    crytoCurrency = models.CharField(max_length=100)
+    price = models.FloatField()
+    status = models.CharField(max_length=50)
 
 class User(models.Model):
-   name = models.CharField(max_length=100)
-   authCode = models.TextField()
+    name = models.CharField(max_length=100)
+    authCode = models.TextField()
