097068ea4e1cda32e8361addbc854903e19cb3b6
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 13:33:49 +0530

    Added crypto currency field

--- price_alert_application/price_alert_api/models.py
+++ price_alert_application/price_alert_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Alert(models.Model):
    name = models.CharField(max_length=100)
+   crytoCurrency = models.CharField(max_length=100)
    price = models.FloatField()
    status = models.CharField(max_length=50)
 
