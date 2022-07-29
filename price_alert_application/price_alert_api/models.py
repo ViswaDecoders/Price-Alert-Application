8fc049cbaa2f8cccd0f1498257e27c94070804a6
Viswanath <alapativiswanath1@gmail.com>
2022-07-29 13:28:20 +0530

    Little modifiction

--- price_alert_application/price_alert_api/models.py
+++ price_alert_application/price_alert_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Alert(models.Model):
    name = models.CharField(max_length=100)
-   price = models.IntegerField()
+   price = models.FloatField()
    status = models.CharField(max_length=50)
 
 class User(models.Model):
